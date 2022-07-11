3e590cab941243e2146ffa111cf04412c888e1e3
Bùi Khánh Huyền <buikhanhhuyena4@gmail.com>
2022-07-11 14:22:24 +0700

    using relative_path

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from assigment_1 import read_json 
 import os
+from pathlib import Path
 
 def main(folder_path):
     folder_name = os.path.basename(folder_path)
     df = read_json.read_folder(folder_path=folder_path)
     df.to_csv(f'{folder_name}.csv', index=None)
 
+
 if __name__ == "__main__":
-    main('C://Users/Admin/Downloads/properties')
+    os.chdir('../properties')
+    cwd = Path.cwd()
+    main(cwd)
+    
+    
+    
+
