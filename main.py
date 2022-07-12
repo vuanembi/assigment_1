e2546a39ae266c0eb194e99d82fe68d5a43b019c
Bùi Khánh Huyền <buikhanhhuyena4@gmail.com>
2022-07-12 10:21:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,4 @@ def main(folder_path):
     read_json.folder_to_csv(folder_path)
 
 if __name__ == "__main__":
-    os.chdir('../properties')
-    cwd = Path.cwd()
-    main(cwd)
+    main('../properties')
